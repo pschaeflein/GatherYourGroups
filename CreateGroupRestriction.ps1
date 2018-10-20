c6f41addb25b9ae6683e8bab2905e26f018af4f8
Paul Schaeflein <paul@schaeflein.net>
2018-10-20 06:17:03 -0500

    SPSNE 2018

---
+++ CreateGroupRestriction.ps1
@@ -0,0 +1,28 @@
+﻿#  Manage who can create Office 365 Groups
+#  https://support.office.com/en-us/article/Manage-who-can-create-office-365-groups-4c46c8cb-17d0-44b5-9776-005fced8e618
+
+Uninstall-Module AzureADPreview
+
+Install-Module AzureADPreview
+
+Import-Module AzureADPreview
+
+Connect-AzureAD
+
+Get-AzureADGroup -Filter "SecurityEnabled eq true"
+
+New-AzureADGroup -DisplayName "Group Creators" -Description "Allowed to create Office 365 Groups" -SecurityEnabled $true -MailEnabled $false -MailNickName "GroupCreators"
+
+
+$Template = Get-AzureADDirectorySettingTemplate | where {$_.DisplayName -eq 'Group.Unified'}
+$Setting = $Template.CreateDirectorySetting()
+New-AzureADDirectorySetting -DirectorySetting $Setting
+
+$Setting = Get-AzureADDirectorySetting -Id (Get-AzureADDirectorySetting | where -Property DisplayName -Value "Group.Unified" -EQ).id
+$Setting["EnableGroupCreation"] = $False
+$Setting["GroupCreationAllowedGroupId"] = (Get-AzureADGroup -SearchString "Group Creators").objectid
+
+Set-AzureADDirectorySetting -Id (Get-AzureADDirectorySetting | where -Property DisplayName -Value "Group.Unified" -EQ).id -DirectorySetting $Setting
+
+
+(Get-AzureADDirectorySetting).Values
---
+++ Naming-Blocking-Classifying.ps1
@@ -0,0 +1,46 @@
+﻿# https://docs.microsoft.com/en-us/azure/active-directory/active-directory-accessmanagement-groups-settings-cmdlets
+
+
+#  Office 365 Groups naming policy
+#  https://support.office.com/en-us/article/Office-365-Groups-naming-policy-6ceca4d3-cad1-4532-9f0f-d469dfbbb552
+
+Uninstall-Module AzureADPreview
+
+Install-Module AzureADPreview
+
+Import-Module AzureADPreview
+
+Connect-AzureAD
+
+#############################################
+# Licensing
+#
+#  Group naming policy requires Azure active directory Premium P1 license for unique users that are members of Office 365 groups.
+#############################################
+
+
+#  Supported Azure AD attributes are [Department], [Company], [Office], 
+#         [StateOrProvince], [CountryOrRegion], [Title], [CountryCode]
+#    During policy creation, the total prefixes and suffixes string length is restricted to 53 characters
+
+# Need to include [GroupName] to indicate where user-entered value goes...
+
+$Setting = Get-AzureADDirectorySetting -Id (Get-AzureADDirectorySetting | where -Property DisplayName -Value "Group.Unified" -EQ).id
+$Setting["PrefixSuffixNamingRequirement"] = "Grp_[GroupName]"
+
+
+# The blocked words check is done after appending the prefixes and suffixes to the user entered group name. 
+# So if user enters ‘s**t’ and ‘Prefix_’ is the naming policy, ‘Prefix_s**t’ will pass. 
+# But if the naming policy is ‘Prefix<space>‘, ‘Prefix s**t’ will fail. Consider this while choosing the right prefixes and suffixes.
+
+$Setting["CustomBlockedWordsList"]="Payroll,CEO,HR"
+
+
+#
+#
+#
+
+$Setting["ClassificationList"] = "High,Medium,Low"
+Set-UnifiedGroup <LowImpactGroup@constoso.com> -Classification <LowImpact> 
+
+Set-AzureADDirectorySetting -Id (Get-AzureADDirectorySetting | where -Property DisplayName -Value "Group.Unified" -EQ).id -DirectorySetting $Setting
